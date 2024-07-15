6abbf5bfd168cdebabda89ee348f29f9b4c89e8d
Mike O'Brien <mike.j.obrien@gmail.com>
2024-07-15 11:11:42 -0700

    Added region variable.

--- terraform-modules/aws/s3/variables.tf
+++ terraform-modules/aws/s3/variables.tf
@@ -5,3 +5,9 @@ variable "buckets" {
     tags = map(string)
   }))
 }
+
+variable "region" {
+  description = "The AWS region to create resources in"
+  type        = string
+  default     = "us-west-1"
+}
