561054081ac65f7bd44bfff879046213641ee9a8
Mike O'Brien <mike.j.obrien@gmail.com>
2024-07-15 11:10:02 -0700

    Added more outputs for ARNs and regions.

--- terraform-modules/aws/s3/outputs.tf
+++ terraform-modules/aws/s3/outputs.tf
@@ -2,3 +2,13 @@ output "bucket_names" {
   description = "The names of the S3 buckets"
   value       = [for bucket in aws_s3_bucket.this : bucket.bucket]
 }
+
+output "bucket_arns" {
+  description = "The ARNs of the S3 buckets"
+  value       = [for bucket in aws_s3_bucket.this : bucket.arn]
+}
+
+output "bucket_regions" {
+  description = "The regions of the S3 buckets"
+  value       = [for bucket in aws_s3_bucket.this : bucket.region]
+}
